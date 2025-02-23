dd24c5284a8e1f7d2717f7ad9322ac85c515dcb0
thuanbudu112 <ledinhthuan27@gmail.com>
2025-02-23 21:26:46 +0700

    t

--- btap_tuan2/src/btap_tuan2/Btap_tuan2.java
+++ btap_tuan2/src/btap_tuan2/Btap_tuan2.java
@@ -17,7 +17,7 @@ public class Btap_tuan2 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-       tinhThuong();
+
     }
     public  static void tinhtong(){
         Scanner sc = new Scanner(System.in);
@@ -28,36 +28,7 @@ public class Btap_tuan2 {
         System.out.println("tong = "+(a+b));
 
     }
-    public static void tinhTich(){
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Nhap so thu nhat: ");
-        double a = sc.nextDouble();
-        System.out.println("Nhap so thu hai: ");
-        double b = sc.nextDouble();
-        
-        double ketQua = a * b;
-        System.out.println("Tich = "+ketQua);
-                
-    }
-    public static void tinhHieu(){
-        Scanner sc = new Scanner(System.in);
-        System.out.println("nhap gia tri a : ");
-        double a = sc.nextDouble();
-        System.out.println("nhap gia tr b : ");
-        double b = sc.nextDouble();
-        double ketqua = a-b;
-        System.out.println("Hieu = "+ketqua);
-    }
-    public static void tinhThuong(){
-        Scanner sc = new Scanner(System.in);
-        System.err.println("Nhap gia tri a : ");
-        double a = sc.nextDouble();
-        System.err.println("Nhap gia tri b : ");
-        double b = sc.nextDouble();
-        double ketQua = a/b;
-        System.out.println("Ket qua = "+ketQua);
-        
-
-    }
+  
+ }
     
-}
+
