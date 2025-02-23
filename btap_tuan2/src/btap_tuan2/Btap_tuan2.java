a3d5f92aa1f37bc0ec5d326cfdb6053d626eec85
hieppro <lehiep08052005@gmail.com>
2025-02-23 15:44:51 +0700

    baitap tuan 2

---
+++ btap_tuan2/src/btap_tuan2/Btap_tuan2.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package btap_tuan2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author yassu
+ */
+public class Btap_tuan2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        tinhtong();
+    }
+    public  static void tinhtong(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("nhap gia tri a: ");
+        int a = sc.nextInt();
+        System.out.println("nhap gia tri b: ");
+        int b = sc.nextInt();
+        System.out.println("tong = "+(a+b));
+
+    }
+    
+}
